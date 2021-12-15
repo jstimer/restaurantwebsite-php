7d0a587d6e36007befc82fff12ef872b9ae06eda
Jerry Stimer <jstimer93@gmail.com>
2021-12-15 14:24:00 -0500

    Add files via upload

---
+++ About_Us.php
@@ -0,0 +1,76 @@
+
+<?php require "Restaurant.inc";	
+
+	StartHTML( "HTML" );
+
+	HTMLHeader();
+
+	HTMLNav();
+?>
+
+
+<div class="body">
+
+<div class="imgTable">
+	<img class="heroes" src="Images/Heroes.jpg" alt="Heroes of the Dorm">
+<table class="images">
+<tbody>
+	<tr>
+	<td><img src="Images/burger.png" alt="burger icon" style="width: 206px; height: 150px;"/></td>
+	<td><img src="Images/service.png" alt="staff" style="width: 206px; height:150px;"></td>	
+	<td><img src="Images/coffee.svg" alt="Not Krabby Patty" style="width: 206px; height:150px;"></td>	
+	</tr>
+</tbody>
+</table>
+
+</div>
+
+<table class="about_us">
+<tbody>
+	<tr>
+		<td align="center" class="tableTitle" valign="top">
+		About Us
+		</td>
+	</tr>	
+	<tr>
+		<td align="center" valign="top">
+			<p>Why Choose Us?</p>
+		</td>
+	</tr>
+	<table class="tableContent" align="center">
+		<tbody>
+			<tr>
+				<td>
+					-Esports Tv
+					<br>
+					-Friendly staff
+				</td>
+				<td>
+					-Great Food
+					<br>
+					-Unique Atmosphere
+				</td>
+					<td>	
+					-Dine-In & Takeout
+					<br>
+					-Famous Cup-O-JAVA
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</tbody>
+</table>
+
+<p class="paragraph">
+	Welcome to HTML, the newest and hippest lunch destination this side of Lock Haven.
+	Hey! That&#39s My Lunch! was founded in 2016 by two visionaries with the sole purpose to provide the greatest lunch variety in town. 
+Built along the winding back of the Susquehanna River, HTML is the first ever building allowed to be physically built on the levee
+ which protects the fine town of Lock Haven. HTML is the first technology themed lunch destination from State College to Williamsport! 
+ The founders of HTML are Lock Haven University&#39s very own Lucas Reid and Jerry Stimer who love lunch and computers so much they merged 
+ the two into a happy medium which we all know and love today as Hey! That&#39s My lunch!
+ </p>
+<p align=center><strong>Stop in today! </strong></p>
+<?php
+	HTMLFooter();
+
+?>
